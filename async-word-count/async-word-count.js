4546111ed2038372e773ff393617b7b3b956a7e2
Justin Le <justinhung.le@gmail.com>
2021-12-11 16:59:39 -0800

    Completed Async-Word-Count

--- async-word-count/async-word-count.js
+++ async-word-count/async-word-count.js
@@ -2,12 +2,13 @@ var fs = require('fs');
 var path = require('path');
 
 var getWordCount = function(filePath, callback) {
+
+  //reads file and get wordcount, then invokes callback topass wordCount
   fs.readFile(filePath, 'utf-8', function(err, data) {
     if (err) {
       callback(err, null);
       return;
     }
-
     var wordCount = data.trim().split(' ').length;
     callback(null, wordCount);
   });
@@ -15,6 +16,21 @@ var getWordCount = function(filePath, callback) {
 
 var getTotalWordCount = function(filePathOne, filePathTwo, callback) {
   // YOUR CODE HERE
+  getWordCount(filePathOne, (err, data) => {
+    if (err) {
+      console.log('Error :', err);
+    } else {
+      let wordCount = data;
+      getWordCount(filePathTwo, (err, dataTwo) => {
+        if (err) {
+          console.log('Error :', err);
+        } else {
+          let total = wordCount + dataTwo;
+          callback(null, total);
+        }
+      })
+    }
+  })
 };
 
 module.exports = getTotalWordCount;
