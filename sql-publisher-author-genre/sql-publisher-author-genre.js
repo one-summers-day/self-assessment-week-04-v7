83f5c5910270acc5c7bb1b38d4bdfe3e03f10dee
ChrisRolling <chrisrolling27@gmail.com>
2021-12-11 17:23:15 -0800

    sql done!

--- sql-publisher-author-genre/sql-publisher-author-genre.js
+++ sql-publisher-author-genre/sql-publisher-author-genre.js
@@ -9,11 +9,16 @@ var Schemas = {
   OPTION_6: 'option 6'
 };
 
-var sqlPublisherAuthorSchema =  Schemas.FIX_ME; // TODO: Update this constant
+var sqlPublisherAuthorSchema =  Schemas.OPTION_6; // TODO: Update this constant
 // TODO: Write a comment here explaining your choice
+// Many to many relationship requires a join table
 
-var sqlPublisherGenreSchema =  Schemas.FIX_ME; // TODO: Update this constant
-// TODO: Write a comment here explaining your choice
 
-var sqlAuthorGenreSchema =  Schemas.FIX_ME; // TODO: Update this constant
+var sqlPublisherGenreSchema =  Schemas.OPTION_4; // TODO: Update this constant
 // TODO: Write a comment here explaining your choice
+// A given publisher will only have one genre and can be referenced by genre ID
+
+
+var sqlAuthorGenreSchema =  Schemas.OPTION_6; // TODO: Update this constant
+// TODO: Write a comment here explaining your choice
+// Many to many relationship requires a join table
