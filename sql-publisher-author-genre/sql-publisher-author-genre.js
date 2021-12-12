63f36bd73fb8ccba479a0890282f47bcf5fb0d2c
Fernanda Rodrigues da Silva <fernanda.rodrigues.cdc@gmail.com>
2021-12-11 16:21:32 -0800

    SQL publisher author genre

--- sql-publisher-author-genre/sql-publisher-author-genre.js
+++ sql-publisher-author-genre/sql-publisher-author-genre.js
@@ -9,11 +9,11 @@ var Schemas = {
   OPTION_6: 'option 6'
 };
 
-var sqlPublisherAuthorSchema =  Schemas.FIX_ME; // TODO: Update this constant
-// TODO: Write a comment here explaining your choice
+var sqlPublisherAuthorSchema =  Schemas.OPTION_6;
+// For as many to many relationship an extra relationship table where the ids for both tables will be joined. This way the table Authors and Schemas don't need any repetions.
 
-var sqlPublisherGenreSchema =  Schemas.FIX_ME; // TODO: Update this constant
-// TODO: Write a comment here explaining your choice
+var sqlPublisherGenreSchema =  Schemas.OPTION_4;
+// On one to many relationships, we only need to add the id of the table row that can be in many rows of the other table to the other table as a foreign key.
 
-var sqlAuthorGenreSchema =  Schemas.FIX_ME; // TODO: Update this constant
-// TODO: Write a comment here explaining your choice
+var sqlAuthorGenreSchema =  Schemas.OPTION_6;
+//  Same as sqlPublisherAuthorSchema
