e70f42e5859b4ed8d1c174ef3a1a20f88cb77be3
bgtdinh <bgtdinh@gmail.com>
2021-12-11 16:49:24 -0800

    Completed getTotal Word count using extra variable in callback

--- async-word-count/async-word-count.js
+++ async-word-count/async-word-count.js
@@ -15,6 +15,22 @@ var getWordCount = function(filePath, callback) {
 
 var getTotalWordCount = function(filePathOne, filePathTwo, callback) {
   // YOUR CODE HERE
+  getWordCount(filePathOne, (err, wordCount) => {
+    if (err) {
+      console.error('Error getting first word count ', err);
+    } else {
+      let firstWordCount = wordCount;
+      getWordCount(filePathTwo, (err, wordCount) => {
+        if (err) {
+          console.error('Error getting second word count ', err);
+        } else {
+          callback(null, wordCount + firstWordCount);
+        }
+      });
+    }
+  });
 };
 
 module.exports = getTotalWordCount;
+
+// getTotalWordCount('/home/brian/hackreactor/hr-sfo139-self-assessment-week-04-v7/async-word-count/files/fileOne.txt', '/home/brian/hackreactor/hr-sfo139-self-assessment-week-04-v7/async-word-count/files/fileTwo.txt', console.log);
