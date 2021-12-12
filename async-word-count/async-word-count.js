681a0071d65a8a25938d5143c7065beec6d4db1b
Brandon Tsai <bjt888@gmail.com>
2021-12-11 16:37:24 -0800

    implemented async-word-count

--- async-word-count/async-word-count.js
+++ async-word-count/async-word-count.js
@@ -15,6 +15,30 @@ var getWordCount = function(filePath, callback) {
 
 var getTotalWordCount = function(filePathOne, filePathTwo, callback) {
   // YOUR CODE HERE
+  getWordCount(filePathOne, (error, countOne) => {
+    if (error) {
+      console.log('error');
+    } else {
+      let totalCount = countOne;
+      getWordCount(filePathTwo, (error, countTwo) => {
+        if (error) {
+          console.log('error2');
+        } else {
+          totalCount += countTwo;
+          console.log(totalCount);
+        }
+      })
+    }
+  })
+
 };
 
 module.exports = getTotalWordCount;
+
+getTotalWordCount('./files/fileOne.txt', './files/fileTwo.txt', (error, data) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(data);
+  }
+});
